61d4b61f18616ad8ff30e050f6bfde6103c22bbd
OptimusJosh <joshuaokwukweogugua@gmail.com>
2023-02-16 12:55:17 -0800

    print size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - C programme that prints a line using the printf function
  *
- * Return: 0 when successfull
+ * Return: 0 (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
